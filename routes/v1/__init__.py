577e246508065bd3ad33768d8a90f10a93e3d6a0
proguy914629 <github@proguy914629.me>
2022-09-08 23:15:06 +0700

    Load lyrics router

--- routes/v1/__init__.py
+++ routes/v1/__init__.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter
 from fastapi.responses import *
 
 from .music import router as music_router
+from .lyrics import router as lyrics_router
 
 
 router = APIRouter()
 routes = [
     ("Music", music_router),
+    ("Lyrics", lyrics_router)
 ]
 
 for name, route in routes:
