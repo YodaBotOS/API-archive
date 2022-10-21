5a5e0b26a6266b93d65d80c0a2521e9c73ebf7ee
proguy914629 <github@proguy914629.me>
2022-10-21 22:38:43 +0700

    Better setup

--- setup.bash
+++ setup.bash
@@ -29,6 +29,6 @@ sudo cp core/setup/nginx.conf /etc/nginx/conf.d/yodaapi.conf
 
 sudo cp core/setup/systemd.service /etc/systemd/system/API.service
 sudo systemctl daemon-reload
-sudo systemctl enable API --now
+sudo systemctl enable API
 
 source ~/.bashrc
