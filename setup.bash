df69cb4ee31a1a0855ad341f9a310e4069f40557
proguy914629 <github@proguy914629.me>
2022-10-21 22:43:27 +0700

    Better setup

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get install python3.10 python3.10-venv python3.10-dev
+sudo apt-get install python3.10 python3.10-venv python3.10-dev -y
 export PATH=$PATH:$HOME/.local/bin
 curl --fail --silent --show-error https://bootstrap.pypa.io/get-pip.py | python3.10
 python3.10 -m pip install -U pip setuptools wheel
