358f7e8a0e83045580246da2088a0a3166335eef
proguy914629 <github@proguy.me>
2022-12-29 16:36:33 +0700

    Fix lyrics
    For some reason...

--- routes/v2/lyrics.py
+++ routes/v2/lyrics.py
@@ -97,7 +97,7 @@ async def search(q: str):
 
     for name, endpoint in images.items():
         url = f'https://{cdn_url}/{endpoint}'
-        url.replace(f'https://{cdn_url}/https://{cdn_url}/', f'https://{cdn_url}/')  # idk why this happens
+        url = url.replace(f'https://{cdn_url}/https://{cdn_url}/', f'https://{cdn_url}/')  # idk why this happens
         i[name] = url
 
     title = res.title
