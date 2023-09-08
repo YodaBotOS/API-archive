74ca89066904619da11f4664060c5df3ce7034b0
proguy914629 <proguy@proguy.me>
2023-09-08 16:07:35 +0000

    Add Google Analytics to API

--- core/app.py
+++ core/app.py
@@ -70,6 +70,44 @@ def add_routes(app: App):
         app.include_router(r, tags=[f"v{name}"], prefix=f"/v/{name}",
                            include_in_schema=False)
 
+def add_google_analytics(app: App):
+    ga_script = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={code}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '{code}');
+</script>
+    """
+    
+    @app.middleware("http")
+    async def insert_js(request: Request, call_next):
+        path = request.scope["path"]  # get the request route
+        response = await call_next(request)
+
+        response_body = ""
+        async for chunk in response.body_iterator:
+            response_body += chunk.decode()
+
+        charset_tag = '<meta charset="utf-8">'
+        color_scheme_tag = '<meta name="color-scheme" content="light dark">'
+        if charset_tag in response_body:
+            response_body = response_body.replace(charset_tag, charset_tag + ga_script.format(code=config.GOOGLE_ANALYTICS_CODE))
+        elif color_scheme_tag in response_body:
+            response_body = response_body.replace(color_scheme_tag, color_scheme_tag + ga_script.format(code=config.GOOGLE_ANALYTICS_CODE))
+        else:
+            response_body = response_body.replace('<head>', '<head>\n' + ga_script.format(code=config.GOOGLE_ANALYTICS_CODE))
+
+        return Response(
+            content=response_body,
+            status_code=response.status_code,
+            headers=dict(response.headers),
+            media_type=response.media_type,
+        )
+
 
 def init_database(app: App):
     # app.db = init_db(sync=True)
@@ -77,7 +115,7 @@ def init_database(app: App):
 
 
 def callback(app: App):
-    funcs = (init_database, add_routes, make_tmp_dir)
+    funcs = (init_database, add_routes, make_tmp_dir, add_google_analytics)
 
     for func in funcs:
         func(app)
